24db92f7f7a728601b7bd61e125ae16c34f75a39
Sourodeep Chatterjee <sourodeep224@outlook.com>
2018-09-25 01:51:04 +0530

    AddedSupportForWavAndMp3InDifferentButtons

--- MediaPlayerWithRecording/Form1.Designer.cs
+++ MediaPlayerWithRecording/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MediaPlayerWithRecording
 {
-    partial class Form1
+    partial class FirstWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ButtonOpenWavFile = new System.Windows.Forms.Button();
+            this.ButtonPlayOrPause = new System.Windows.Forms.Button();
+            this.ButtonOpenMp3File = new System.Windows.Forms.Button();
+            this.ButtonPlayOrPauseMp3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonOpenWavFile
+            // 
+            this.ButtonOpenWavFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonOpenWavFile.FlatAppearance.BorderSize = 0;
+            this.ButtonOpenWavFile.Location = new System.Drawing.Point(13, 13);
+            this.ButtonOpenWavFile.Name = "ButtonOpenWavFile";
+            this.ButtonOpenWavFile.Size = new System.Drawing.Size(385, 33);
+            this.ButtonOpenWavFile.TabIndex = 0;
+            this.ButtonOpenWavFile.Text = "Open WAV File";
+            this.ButtonOpenWavFile.UseVisualStyleBackColor = false;
+            this.ButtonOpenWavFile.Click += new System.EventHandler(this.ButtonOpenWavFile_Click);
+            // 
+            // ButtonPlayOrPause
+            // 
+            this.ButtonPlayOrPause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonPlayOrPause.Enabled = false;
+            this.ButtonPlayOrPause.FlatAppearance.BorderSize = 0;
+            this.ButtonPlayOrPause.Location = new System.Drawing.Point(12, 61);
+            this.ButtonPlayOrPause.Name = "ButtonPlayOrPause";
+            this.ButtonPlayOrPause.Size = new System.Drawing.Size(385, 33);
+            this.ButtonPlayOrPause.TabIndex = 0;
+            this.ButtonPlayOrPause.Text = "Play/Pause WAV";
+            this.ButtonPlayOrPause.UseVisualStyleBackColor = false;
+            this.ButtonPlayOrPause.Click += new System.EventHandler(this.ButtonPlayOrPause_Click);
+            // 
+            // ButtonOpenMp3File
+            // 
+            this.ButtonOpenMp3File.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonOpenMp3File.FlatAppearance.BorderSize = 0;
+            this.ButtonOpenMp3File.Location = new System.Drawing.Point(13, 110);
+            this.ButtonOpenMp3File.Name = "ButtonOpenMp3File";
+            this.ButtonOpenMp3File.Size = new System.Drawing.Size(385, 33);
+            this.ButtonOpenMp3File.TabIndex = 0;
+            this.ButtonOpenMp3File.Text = "Open MP3 File";
+            this.ButtonOpenMp3File.UseVisualStyleBackColor = false;
+            this.ButtonOpenMp3File.Click += new System.EventHandler(this.ButtonOpenMp3File_Click);
+            // 
+            // ButtonPlayOrPauseMp3
+            // 
+            this.ButtonPlayOrPauseMp3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonPlayOrPauseMp3.Enabled = false;
+            this.ButtonPlayOrPauseMp3.FlatAppearance.BorderSize = 0;
+            this.ButtonPlayOrPauseMp3.Location = new System.Drawing.Point(12, 160);
+            this.ButtonPlayOrPauseMp3.Name = "ButtonPlayOrPauseMp3";
+            this.ButtonPlayOrPauseMp3.Size = new System.Drawing.Size(385, 33);
+            this.ButtonPlayOrPauseMp3.TabIndex = 0;
+            this.ButtonPlayOrPauseMp3.Text = "Play/Pause MP3";
+            this.ButtonPlayOrPauseMp3.UseVisualStyleBackColor = false;
+            this.ButtonPlayOrPauseMp3.Click += new System.EventHandler(this.ButtonPlayOrPause_Click);
+            // 
+            // FirstWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(410, 268);
+            this.Controls.Add(this.ButtonPlayOrPauseMp3);
+            this.Controls.Add(this.ButtonPlayOrPause);
+            this.Controls.Add(this.ButtonOpenMp3File);
+            this.Controls.Add(this.ButtonOpenWavFile);
+            this.Name = "FirstWindow";
+            this.Text = "First Media Player";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FirstWindow_FormClosing);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonOpenWavFile;
+        private System.Windows.Forms.Button ButtonPlayOrPause;
+        private System.Windows.Forms.Button ButtonOpenMp3File;
+        private System.Windows.Forms.Button ButtonPlayOrPauseMp3;
     }
 }
 
--- MediaPlayerWithRecording/Form1.cs
+++ MediaPlayerWithRecording/Form1.cs
@@ -1,20 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NAudio.Wave;
+using System;
 using System.Windows.Forms;
 
 namespace MediaPlayerWithRecording
 {
-    public partial class Form1 : Form
+    public partial class FirstWindow : Form
     {
-        public Form1()
+        public FirstWindow()
         {
             InitializeComponent();
+        }
+
+        // For Wave File input
+        private WaveFileReader _wave;
+
+        // For Mp3 input
+        private BlockAlignReductionStream _stream;
+
+        private DirectSoundOut _output;
+
+        private void ButtonOpenWavFile_Click(object sender, EventArgs e)
+        {
+            var open = new OpenFileDialog { Filter = "Wave File (*.wav)|*.wav" };
+            if (open.ShowDialog() != DialogResult.OK)
+                return;
+            DisposeWave();
+
+            // Input logic for WAV
+            _wave = new WaveFileReader(open.FileName);
+
+            // Output logic
+            _output = new DirectSoundOut();
+            _output.Init(new WaveChannel32(_wave));
+            _output.Play();
+
+            ButtonPlayOrPause.Enabled = true;
+        }
+
+        private void ButtonPlayOrPause_Click(object sender, EventArgs e)
+        {
+            if (_output == null) return;
+            switch (_output.PlaybackState)
+            {
+                case PlaybackState.Playing:
+                    _output.Pause();
+                    break;
+
+                case PlaybackState.Paused:
+                    _output.Play();
+                    break;
+
+                case PlaybackState.Stopped:
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void ButtonOpenMp3File_Click(object sender, EventArgs e)
+        {
+            var open = new OpenFileDialog { Filter = "MP3 File (*.mp3)|*.mp3" };
+            if (open.ShowDialog() != DialogResult.OK)
+                return;
+            DisposeMp3();
+
+            // Input logic for mp3
+            var pcm = WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(open.FileName));
+            _stream = new BlockAlignReductionStream(pcm);
+
+            // Output logic
+            _output = new DirectSoundOut();
+            _output.Init(new WaveChannel32(_stream));
+            _output.Play();
+
+            ButtonPlayOrPauseMp3.Enabled = true;
+        }
+
+        private void DisposeWave()
+        {
+            if (_output == null) return;
+
+            if (_output.PlaybackState == PlaybackState.Playing)
+                _output.Stop();
+
+            _output.Dispose();
+            _output = null;
+
+            if (_wave == null) return;
+
+            _wave.Dispose();
+            _wave = null;
+        }
+
+        private void DisposeMp3()
+        {
+            if (_output == null) return;
+
+            if (_output.PlaybackState == PlaybackState.Playing)
+                _output.Stop();
+
+            _output.Dispose();
+            _output = null;
+
+            if (_stream == null) return;
+
+            _stream.Dispose();
+            _stream = null;
+        }
+
+        private void FirstWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisposeWave();
+            DisposeMp3();
         }
     }
 }
